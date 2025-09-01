3e3e403a6484503549a77b5a8813ea0ac237237b
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-09-01 14:10:08 -0300

    feat: melhorias nas responsividades

--- js/about.js
+++ js/about.js
@@ -4,9 +4,9 @@ const modalImg = document.getElementById("modal-img");
 const caption = document.getElementById("caption");
 const closeBtn = document.querySelector(".close");
 
-document.querySelectorAll(".team-photo").forEach(img => {
+document.querySelectorAll(".team-photo").forEach((img) => {
   img.addEventListener("click", () => {
-    modal.style.display = "block";
+    modal.style.display = "flex"; 
     modalImg.src = img.src;
     caption.innerHTML = img.alt;
   });
@@ -14,4 +14,10 @@ document.querySelectorAll(".team-photo").forEach(img => {
 
 closeBtn.addEventListener("click", () => {
   modal.style.display = "none";
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
